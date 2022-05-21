5c752312b33d1a23c9b83319e98746fa9efe7cb7
Brandon <brandonkanute@gmail.com>
2022-05-21 23:04:08 +0300

    Completed Movie show and search section

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+import MovieList from './MovieList';
+import MovieListHeading from './MovieListHeading';
+import SearchBox from './SearchBox';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
+	const getMovieRequest = async (searchValue) => {
+		const url = `http://www.omdbapi.com/?s=${searchValue}&apikey=263d22d8`;
+
+		const response = await fetch(url);
+		const responseJson = await response.json();
+
+		if (responseJson.Search) {
+			setMovies(responseJson.Search);
+			console.log(movies);
+		}
+	};
+
+	useEffect(() => {
+		getMovieRequest(searchValue);
+	}, [searchValue]);
+
+	return (
+		<div className='container-fluid movie-app'>
+			<div className='row'>
+				<MovieListHeading heading='Netlify' />
+				<SearchBox searchValue={searchValue} setSearchValue={setSearchValue} />
+			</div>
+			<div className='row'>
+				<MovieList movies={movies} />
+			</div>
+		</div>
+	);
+};
+
+export default App;
---
+++ src/components/MovieList.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const MovieList = (props) => {
+	return (
+		<>
+			{props.movies.map((movie, index) => (
+				<div className='image-container'>
+					<img src={movie.Poster} alt='movie'></img>
+					<h3>{movie.Title}</h3>
+					<h4>{movie.Year}</h4>
+				</div>
+			))}
+		</>
+	);
+};
+
+export default MovieList;
